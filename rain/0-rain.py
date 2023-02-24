3dd77c7041b71bdcb9f79b86ebe28a7483728fd8
gichuri <m.ndungu@alustudent.com>
2023-02-24 21:13:43 +0300

    add

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -5,20 +5,23 @@
 
 def rain(walls):
     """ function that calculates water retained"""
-    n = len(walls)
-    left = [0] * n
-    right = [0] * n
+    if len(walls) == 0: 
+        continue
+    else:
+        n = len(walls)
+        left = [0] * n
+        right = [0] * n
 
-    left[0] = walls[0]
-    for i in range(1, n):
-        left[i] = max(left[i-1], walls[i])
+        left[0] = walls[0]
+        for i in range(1, n):
+            left[i] = max(left[i-1], walls[i])
 
-    right[n-1] = walls[n-1]
-    for i in range(n-2, -1, -1):
-        right[i] = max(right[i+1], walls[i])
+        right[n-1] = walls[n-1]
+        for i in range(n-2, -1, -1):
+            right[i] = max(right[i+1], walls[i])
 
-    water = 0
-    for i in range(n):
-        water += max(0, min(left[i], right[i]) - walls[i])
+        water = 0
+        for i in range(n):
+            water += max(0, min(left[i], right[i]) - walls[i])
 
-    return water
+        return water
