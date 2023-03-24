559db28d5029c64f7368464de7d2bf8b7fdfb84b
gichuri <m.ndungu@alustudent.com>
2023-03-25 01:09:57 +0300

    pascal

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,17 @@
 
 #!/usr/bin/python3
-"""Comment"""
+"""
+Print Pascal's Triangle
+"""
 
 
 def pascal_triangle(n):
-    """Comment"""
-    if n <= 0:
-        return []
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
-    return 
+    """returns Pascal's triangle in a list of list"""
+    my_list = []
+    if (n <= 0):
+        return my_list
+    my_list.append([1])
+    for i in range(n - 1):
+        my_list.append([1] + [my_list[i][a] + my_list[i][a + 1]
+                              for a in range(len(my_list[i]) - 1)] + [1])
+    return my_list
